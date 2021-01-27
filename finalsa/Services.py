337973513cc9979a1b309c31158dc5eaab06075d
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-01-27 11:56:07 -0600

    Update Services.py

--- finalsa/Services.py
+++ finalsa/Services.py
@@ -3,11 +3,11 @@ from json import dumps
 from datetime import datetime
 from .Exceptions import AuthException
 
+
 class Services():
 
     url = ''
     api_key = ''
-
 
     def __init__(self, url, api_key):
         super().__init__()
@@ -22,14 +22,13 @@ class Services():
         }
         url = self.url + '/session_login/'
         payload = {
-            'attributes' : self.api_key,
+            'attributes': self.api_key,
         }
         r = request_to_api(url, "POST", headers, payload)
         if('auth_token' in r):
             self.auth_date = datetime.now()
             return r['auth_token']
         raise AuthException()
-
 
     def get_headers(self, ):
         headers = {
@@ -38,32 +37,42 @@ class Services():
             'Auth': self.auth_key
         }
         return headers
-    
+
     def send_campaing(self, typ, service_id, webhook, template, name, attributes):
         headers = self.get_headers()
         url = self.url + '/create_campaing/'
         payload = {
-            'attributes' : attributes,
-            'type' : typ,
-            'service_id' : service_id,
-            'webhook' : webhook,
-            'template' : template,
-            'name' : name
+            'attributes': attributes,
+            'type': typ,
+            'service_id': service_id,
+            'webhook': webhook,
+            'template': template,
+            'name': name
         }
         r = request_to_api(url, "POST", headers, payload)
         return r
 
-    def send_message(self, to, content, sender = ''):
-        to = verify_phone(to)
-        url = self.url + '/service/'
+    def call_service(self, service_id, payload):
+        url = self.url + '/service/{}'.format(str(service_id))
+        headers = self.get_headers()
+        r = request_to_api(url, "POST", headers, payload)
+        return r
+
+    def send_sms(self, to, message):
+        url = self.url + '/sms/'
         headers = self.get_headers()
         payload = {
-            'tel': to,
-            'msj': content
+            'to' : to,
+            'message' : message
         }
         r = request_to_api(url, "POST", headers, payload)
         return r
-    
+
+    def send_call(self, payload):
+        url = self.url + '/call/'
+        headers = self.get_headers()
+        r = request_to_api(url, "POST", headers, payload)
+        return r
 
     def single_recharge(self, type_id,  phone):
         url = self.url + '/recharge/'
@@ -71,11 +80,11 @@ class Services():
         payload = {
             'recharge_type_id': type_id,
             'phone': phone,
-            'should_wait_time' : 5
+            'should_wait_time': 5
         }
         r = request_to_api(url, "POST", headers, payload)
         return r
-    
+
     def get_recharge_types(self,):
         url = self.url + '/recharge_types/'
         headers = self.get_headers()
